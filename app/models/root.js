c5d0e9299a212736c959e9133fad20898bacbfd9
Tarun Arora <tananrules@gmail.com>
2014-07-22 15:58:54 +0530

    i1

--- app/models/root.js
+++ app/models/root.js
@@ -2,6 +2,4 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	pretty_name: DS.attr('string'),
-	taxonomy: DS.belongsTo('taxonomy'),
-	taxons: DS.hasMany('taxon')
 });
--- app/models/taxon.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-	name: DS.attr('string'),
-	pretty_name: DS.attr('string')
-});
